a6eeed66b5111d14c1b5eb798637ec1e026b5132
Peter Tomulec <tvoj@email.com>
2025-02-08 16:08:33 +0100

    update

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,6 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAuth, connectAuthEmulator } from "firebase/auth";
+import { getAuth, browserLocalPersistence, setPersistence } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -23,7 +23,13 @@ export const auth = getAuth(app);
 export const db = getFirestore(app);
 
 // Nastav persistence na LOCAL
-auth.setPersistence('LOCAL');
+setPersistence(auth, browserLocalPersistence)
+  .then(() => {
+    console.log('Persistence set to LOCAL');
+  })
+  .catch((error) => {
+    console.error('Error setting persistence:', error);
+  });
 
 // Debug logy
 auth.onAuthStateChanged((user) => {
